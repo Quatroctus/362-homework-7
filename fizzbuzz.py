7be5a097d53cd176f42ffdcbe8cd4641946ed68e
Anime-Quatroctus <kanadetachibona@gmail.com>
2021-05-31 20:55:39 -0700

    Fizzbuzz now succeeds the third test.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,5 +3,7 @@ def fizzbuzz():
     for i in range(1, 101, 1):
         if i % 3 == 0:
             print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
         else:
             print(i)
