60b29431e6181de7784942c53db8c93603486ed1
Anime-Quatroctus <kanadetachibona@gmail.com>
2021-05-31 21:19:34 -0700

    Added second test for leapyear not being multiple of 100.

--- test_leapyear.py
+++ test_leapyear.py
@@ -6,3 +6,9 @@ def test_non():
     assert leapyear(1998) == False
     assert leapyear(1997) == False
 
+
+def test_leapyear100fail():
+    assert leapyear(1900) == False
+    assert leapyear(1800) == False
+    assert leapyear(1700) == False
+
